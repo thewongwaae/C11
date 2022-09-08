88ccfd9eacbefc0ccce2865c619baab462e84a73
Hong You Wong <hwong@u81z05s03.42kl.edu.my>
2022-09-08 09:51:22 +0800

    small change to file

--- ex03/ft_count_if.c
+++ ex03/ft_count_if.c
@@ -6,20 +6,45 @@
 /*   By: hwong <hwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 21:15:12 by hwong             #+#    #+#             */
-/*   Updated: 2022/09/08 07:57:30 by hwong            ###   ########.fr       */
+/*   Updated: 2022/09/08 09:50:32 by hwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_count_if(char **tab, int length, int (*f)(char *))
 {
 	int	i;
+	int	count;
 
 	i = 0;
-	while (tab[i])
+	count = 0;
+	while (i < length)
 	{
 		if ((*f)(tab[i]))
-			i++;
-		else
-			return (0);
+			count++;
+		i++;
 	}
+	return (count);
 }
+
+/*
+int	ft(char *str)
+{
+	if (*str == 'A')
+		return (1);
+	return (0);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int	main(void)
+{
+	char	**array;
+
+	array = malloc(4 * sizeof(char *));
+	array[0] = "A";
+	array[1] = "Z";
+	array[2] = "A";
+	array[3] = "NULL";
+	printf("%d\n", ft_count_if(array, 3, ft));
+}
+*/
